69dd0fd05d58116b7283ad2f14fd884d62543aa8
Declan Curran <dcrn@protonmail.com>
2017-08-21 09:43:52 +0900

    Updated version

--- mattermost_encryption.user.js
+++ mattermost_encryption.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mattermost Encryption
 // @namespace    https://github.com/dcrn
-// @version      0.1
+// @version      1.0
 // @description  Add encryption to mattermost
 // @author       dcrn
 // @match        http://localhost:8065/*
